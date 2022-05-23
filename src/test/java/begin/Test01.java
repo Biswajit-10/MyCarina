55d404af8c8d4302202157c7eeeef4bf5cb06aa4
Biswajit-10 <https://github.com/Biswajit-10/A.git>
2022-05-23 23:14:20 +0530

    Experiment Passed

---
+++ src/test/java/begin/Test01.java
@@ -0,0 +1,31 @@
+package begin;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.core.foundation.utils.mobile.IMobileUtils;
+import com.qaprosoft.carina.core.foundation.webdriver.DriverHelper;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+
+public class Test01 implements IAbstractTest, IMobileUtils{
+	
+	@Test()
+	public void test002() {
+
+		DriverHelper driverHelper = new DriverHelper(getDriver());
+		ExtendedWebElement btn9 = driverHelper.findExtendedWebElement(By.id("com.miui.calculator:id/btn_9_s"));
+		Assert.assertTrue(btn9.isElementPresent(), "Btn 9 is not present" );
+		btn9.click();
+		ExtendedWebElement multiply = driverHelper.findExtendedWebElement(By.id("com.miui.calculator:id/btn_mul_s"));
+		Assert.assertTrue(multiply.isElementPresent(), "Btn Multiply is not present" );
+	    multiply.click();
+		ExtendedWebElement btn6 = driverHelper.findExtendedWebElement(By.id("com.miui.calculator:id/btn_6_s"));
+		btn6.click();
+		ExtendedWebElement equals = driverHelper.findExtendedWebElement(By.id("com.miui.calculator:id/btn_equal_s"));
+		equals.click();
+
+	}
+
+}
